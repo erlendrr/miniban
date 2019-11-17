62fd1f3d88195998ff30ec50faa19051ce7aa6c6
Erlend Ryan <erlendryan@gmail.com>
2019-11-17 02:05:37 +0100

    det funke

--- bin/ban.sh
+++ bin/ban.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+cd "${0%/*}"; cd ..; MYDIR=$(pwd)
+
 IP=$1
-DBFILE="/home/bruker/miniban/miniban.db"
 TIMESTAMP=$(date +%s)
 
-if [[ $(grep $IP $DBFILE) ]]; then
-#skriv exit "string" for å teste om skriptet fungerer
-exit
-else
-echo $IP,$TIMESTAMP  >> $DBFILE
-echo "$IP har blitt blokkert fra SSH"
-fi
-
+banning() {
+        echo $IP,$TIMESTAMP  >> $MYDIR/miniban.db
+        sed -i /$IP/d $MYDIR/kickcount.db
+        echo "$IP har blitt blokkert fra SSH"
+}
 
 if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
         sudo iptables -A INPUT -p tcp -s $IP --dport 22 -j DROP
+        banning
 elif [[ $IP =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
         sudo ip6tables -A INPUT -p tcp -s $IP --dport 22 -j DROP
+        banning
+else
+        echo "skriv inn gyldig ip"
 fi
--- bin/miniban.sh
+++ bin/miniban.sh
@@ -1,32 +1,28 @@
 #!/bin/bash
 cd "${0%/*}"; cd ..; MYDIR=$(pwd)
 
-getBanCount() {
-cat $MYDIR/bancounter.db
-}
-
 while true; do
+    #henter feilede påloggingsforsøk fra ssh-loggen med X sekunder mellomrom"
     PASSFAILS_1=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | uniq -c)
     sleep 3
-    clear
     PASSFAILS_2=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | uniq -c)
 
+    #Deler opp og sammenligner alle forsøkene
     echo $PASSFAILS_1 | while IFS=" " read -r COUNT_1 IP; do
         echo $PASSFAILS_2 | while IFS=" " read -r COUNT_2 _IP; do
-        echo "count_1: $COUNT_1"
-        echo "count_2: $COUNT_2"
-        echo "ip: $IP"
 
+        #Hvis antall feilede forsøk har økt vil Ip'en bli lagt til i kickcount listen
         if [[ $COUNT_2 -gt $COUNT_1 ]]; then
-                FAILS=$(expr $COUNT_2 - $COUNT_1)
-                echo $FAILS $IP >> $MYDIR/bancounter.db
-                
-            fi
+            echo $IP >> $MYDIR/kickcount.db
+            echo "IP kastet ut"
+        fi
+
+        #Hvis antall feilede forsøk i kickcount listen er over 3 for en vilkårlig IP adresse, vil den kjøres i ban-scriptet
+        cat $MYDIR/kickcount.db | sort | uniq -c | while IFS=" " read -r DB_COUNT DB_IP; do
+                if [[ $DB_COUNT -gt 2 ]]; then
+                    $MYDIR/bin/ban.sh "$DB_IP"
+                fi
+            done
         done
     done
 done
-#echo $PASSFAILS_2 | while IFS=" " read -r COUNT_2 IP_2; do
-   #         if [[ $COUNT_2 -gt $COUNT_1 ]]; then
-  #          echo $IP
- #           fi
-#        done
