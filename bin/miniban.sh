bbfeb80c9450021744f102182c4345fc36740062
Erlend Ryan <erlendryan@gmail.com>
2019-11-20 14:13:14 +0100

    fikset

--- bin/miniban.sh
+++ bin/miniban.sh
@@ -3,28 +3,22 @@ cd "${0%/*}"
 cd ..
 MYDIR=$(pwd)
 
-trap "exit" INT TERM ERR
-trap "kill 0" EXIT
+trap 'kill $(jobs -p)' EXIT TERM
 $MYDIR/bin/unban.sh &
 
-
 while true; do
-    #henter feilede påloggingsforsøk fra ssh-loggen med X sekunder mellomrom"
-    PASSFAILS_1=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | uniq -c)
+    PASSFAILS_1=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | sort | uniq -c)
     sleep 0.2
-    PASSFAILS_2=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | uniq -c)
+    PASSFAILS_2=$(journalctl -u ssh | grep "Failed password" | awk '{print $11}' | sort | uniq -c)
 
-    #Deler opp og sammenligner alle forsøkene
-    echo $PASSFAILS_1 | while IFS=" " read -r COUNT_1 IP; do
-        echo $PASSFAILS_2 | while IFS=" " read -r COUNT_2 _IP; do
+    echo $PASSFAILS_1 | tr " " "\n" | paste -d ' ' - - | while read COUNT_1 IP; do
+        echo $PASSFAILS_2 | tr " " "\n" | paste -d ' ' - - | while read COUNT_2 _IP; do
 
-            #Hvis antall feilede forsøk har økt vil Ip'en bli lagt til i kickcount listen
-            if [[ ($COUNT_2 -gt $COUNT_1) && ($(grep -c $IP $MYDIR/miniban.db) -eq 0) ]]; then
+            if [[ ($COUNT_2 -gt $COUNT_1) && ($IP == $_IP) && ($(grep -c $IP $MYDIR/miniban.db) -eq 0) ]]; then
                 echo $IP >>$MYDIR/kickcount.db
                 echo "$IP kastet ut"
             fi
 
-            #Hvis antall feilede forsøk i kickcount listen er over 3 for en vilkårlig IP adresse, vil den kjøres i ban-scriptet
             cat $MYDIR/kickcount.db | sort | uniq -c | while IFS=" " read -r DB_COUNT DB_IP; do
                 if [[ $DB_COUNT -gt 2 ]]; then
                     echo "ban"
--- bin/unban.sh
+++ bin/unban.sh
@@ -5,7 +5,7 @@ MYDIR=$(pwd)
 
 unban() {
         if [[ $IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
-                sudo iptables -D INPUT -p tcp -s $IP --dport 22 -j REJECT
+                sudo iptables -D INtcp -s $IP --dport 22 -j REJECT
         elif [[ $IP =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
                 sudo ip6tables D INPUT -p tcp -s $IP --dport 22 -j REJECT
         else
@@ -17,7 +17,7 @@ unban() {
 
 while true; do
         #velg sekunder
-        UNBAN_DATE=$(expr $(date +%s) - 20)
+        UNBAN_DATE=$(expr $(date +%s) - 10)
         IFS=$'\n'
         for i in $(cat <$MYDIR/miniban.db); do
                 IFS="," read IP TIMESTAMP
